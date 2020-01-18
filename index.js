d356048043c7f47fd43079be97f727c4d97b7c94
tamilsaran <45892222+tamilsaran@users.noreply.github.com>
2020-01-18 15:38:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const http = require('http');
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello World\n');
+#const server = http.createServer((req, res) => {
+#    res.statusCode = 200;
+#res.setHeader('Content-Type', 'text/plain');
+#res.end('Hello World\n');
+#});
+app.get('/',function(req, res){
+	res.sendfile('index.html');
 });
 
 server.listen(port, () => {

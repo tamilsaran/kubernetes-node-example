bb82a98f344c46d3eb61d6db4d107485625052a1
tamilsaran <45892222+tamilsaran@users.noreply.github.com>
2020-01-18 15:44:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const http = require('http');
-
-const port = process.env.PORT || 3000;
-
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
 
 app.get('/',function(req, res){
 	res.sendfile('index.html');
 });
 
-server.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
+
+app.listen(3000, function () {
+	console.log("Server running at http://0.0.0.0:3000");
 });
